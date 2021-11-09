de2aa2ff612f8ec7aad9bd571991b01e2c085580
Maciej Zubala <maciej.zubala@gmail.com>
2021-11-09 14:31:26 +0100

    exceptions solution

--- side-effects/src/main/java/pl/com/bottega/functional/accounts/DepositFundsHandler.java
+++ side-effects/src/main/java/pl/com/bottega/functional/accounts/DepositFundsHandler.java
@@ -23,7 +23,7 @@ class DefaultDepositFundsHandler implements DepositFundsHandler {
     @Override
     public void handle(DepositFundsCommand command) {
         var account = accountRepository.find(command.getDestination());
-        account.credit(command.getAmount());
+        account.credit(command.getAmount()).get();
         accountRepository.save(account);
     }
 }
--- side-effects/src/test/java/pl/com/bottega/functional/accounts/MoneyTest.java
+++ side-effects/src/test/java/pl/com/bottega/functional/accounts/MoneyTest.java
@@ -1,11 +1,12 @@
 package pl.com.bottega.functional.accounts;
 
+import io.vavr.control.Try;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.util.function.Supplier;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.assertj.vavr.api.VavrAssertions.assertThat;
 import static pl.com.bottega.functional.accounts.ExampleTestData.FIVE_USD;
 import static pl.com.bottega.functional.accounts.ExampleTestData.TEN_EUR;
 import static pl.com.bottega.functional.accounts.ExampleTestData.TEN_USD;
@@ -16,45 +17,44 @@ class MoneyTest {
     @Test
     void addsMoney() {
         // expect
-        assertThat(TEN_USD.add(FIVE_USD)).isEqualTo(new Money(new BigDecimal("15"), USD));
+        assertThat(TEN_USD.add(FIVE_USD)).contains(new Money(new BigDecimal("15"), USD));
     }
 
     @Test
     void subtractsMoney() {
         // expect
-        assertThat(TEN_USD.subtract(FIVE_USD)).isEqualTo(FIVE_USD);
+        assertThat(TEN_USD.subtract(FIVE_USD)).contains(FIVE_USD);
     }
 
     @Test
     void comparesMoney() {
         // expect
-        assertThat(TEN_USD).isGreaterThan(FIVE_USD);
-        assertThat(TEN_USD.isGreaterThan(FIVE_USD)).isTrue();
-        assertThat(TEN_USD).isGreaterThanOrEqualTo(FIVE_USD);
-        assertThat(TEN_USD.isGreaterThanOrEqualTo(FIVE_USD)).isTrue();
-        assertThat(TEN_USD).isGreaterThanOrEqualTo(TEN_USD);
-        assertThat(TEN_USD.isGreaterThanOrEqualTo(TEN_USD)).isTrue();
-        assertThat(FIVE_USD).isLessThan(TEN_USD);
-        assertThat(FIVE_USD.isLessThan(TEN_USD)).isTrue();
-        assertThat(FIVE_USD).isLessThanOrEqualTo(TEN_USD);
-        assertThat(FIVE_USD.isLessThanOrEqualTo(TEN_USD)).isTrue();
-        assertThat(FIVE_USD).isLessThanOrEqualTo(FIVE_USD);
-        assertThat(FIVE_USD.isLessThanOrEqualTo(FIVE_USD)).isTrue();
+        assertThat(TEN_USD.isGreaterThan(FIVE_USD)).contains(true);
+        assertThat(TEN_USD.isGreaterThan(FIVE_USD)).contains(true);
+        assertThat(TEN_USD.isGreaterThanOrEqualTo(FIVE_USD)).contains(true);
+        assertThat(TEN_USD.isGreaterThanOrEqualTo(FIVE_USD)).contains(true);
+        assertThat(TEN_USD.isGreaterThanOrEqualTo(TEN_USD)).contains(true);
+        assertThat(TEN_USD.isGreaterThanOrEqualTo(TEN_USD)).contains(true);
+        assertThat(FIVE_USD.isLessThan(TEN_USD)).contains(true);
+        assertThat(FIVE_USD.isLessThan(TEN_USD)).contains(true);
+        assertThat(FIVE_USD.isLessThanOrEqualTo(TEN_USD)).contains(true);
+        assertThat(FIVE_USD.isLessThanOrEqualTo(TEN_USD)).contains(true);
+        assertThat(FIVE_USD.isLessThanOrEqualTo(FIVE_USD)).contains(true);
+        assertThat(FIVE_USD.isLessThanOrEqualTo(FIVE_USD)).contains(true);
     }
 
     @Test
     void cannotPerformOperationsOnMoneyInDifferentCurrencies() {
         // expect
-        assertIncompatibleCurrencieException(() -> TEN_USD.add(TEN_EUR));
-        assertIncompatibleCurrencieException(() -> TEN_USD.subtract(TEN_EUR));
-        assertIncompatibleCurrencieException(() -> TEN_USD.compareTo(TEN_EUR));
-        assertIncompatibleCurrencieException(() -> TEN_USD.isLessThan(TEN_EUR));
-        assertIncompatibleCurrencieException(() -> TEN_USD.isLessThanOrEqualTo(TEN_EUR));
-        assertIncompatibleCurrencieException(() -> TEN_USD.isGreaterThan(TEN_EUR));
-        assertIncompatibleCurrencieException(() -> TEN_USD.isGreaterThanOrEqualTo(TEN_EUR));
+        assertIncompatibleCurrenciesException(() -> TEN_USD.add(TEN_EUR));
+        assertIncompatibleCurrenciesException(() -> TEN_USD.subtract(TEN_EUR));
+        assertIncompatibleCurrenciesException(() -> TEN_USD.isLessThan(TEN_EUR));
+        assertIncompatibleCurrenciesException(() -> TEN_USD.isLessThanOrEqualTo(TEN_EUR));
+        assertIncompatibleCurrenciesException(() -> TEN_USD.isGreaterThan(TEN_EUR));
+        assertIncompatibleCurrenciesException(() -> TEN_USD.isGreaterThanOrEqualTo(TEN_EUR));
     }
 
-    private void assertIncompatibleCurrencieException(Runnable runnable) {
-        assertThatThrownBy(runnable::run).isInstanceOf(IncompatibleCurrenciesException.class);
+    private void assertIncompatibleCurrenciesException(Supplier<Try<?>> supplier) {
+        assertThat(supplier.get()).isFailure().failBecauseOf(IncompatibleCurrenciesException.class);
     }
 }
