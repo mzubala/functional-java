bc5d2fa211b5fad6b9b46bfaec14316fa7fa215d
Maciej Zubala <maciej.zubala@gmail.com>
2021-11-15 08:43:43 +0100

    transactions handling

--- side-effects/src/main/java/pl/com/bottega/functional/accounts/TransactionalHandler.java
+++ side-effects/src/main/java/pl/com/bottega/functional/accounts/TransactionalHandler.java
@@ -1,8 +1,6 @@
 package pl.com.bottega.functional.accounts;
 
-import org.springframework.r2dbc.connection.R2dbcTransactionManager;
 import org.springframework.transaction.reactive.TransactionalOperator;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 import pl.com.bottega.functional.accounts.Handler.Command;
 import reactor.core.publisher.Mono;
 
@@ -17,7 +15,6 @@ class TransactionalHandler<CommandType extends Command> extends HandlerDecorator
 
     @Override
     public Mono<Void> handle(CommandType command) {
-        return //transactionalOperator.execute(status -> decorated.handle(command)).then();
-            decorated.handle(command);
+        return transactionalOperator.execute(status -> decorated.handle(command)).then();
     }
 }
