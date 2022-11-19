2fcc6df664a6d4839d0b3ab133ea361b2adbc83d
Maciej Zubala <maciej.zubala@gmail.com>
2022-11-19 21:55:48 +0100

    h2

--- side-effects/src/test/resources/schema.sql
+++ side-effects/src/test/resources/schema.sql
@@ -1,18 +1,18 @@
-create table customers
+create table "customers"
 (
-    id         uuid primary key,
-    first_name text   not null,
-    last_name  text   not null,
-    version    bigint not null
+    "id"         uuid primary key,
+    "first_name" text   not null,
+    "last_name"  text   not null,
+    "version"    bigint not null
 );
 
-create table accounts
+create table "accounts"
 (
-    number        character varying(17) primary key,
-    customer_id   uuid   not null references customers (id),
-    currency_code character(3),
-    balance       numeric(2),
-    version       bigint not null
+    "number"        character varying(17) primary key,
+    "customer_id"   uuid   not null references "customers" ("id"),
+    "currency_code" character(3),
+    "balance"       numeric(2),
+    "version"       bigint not null
 );
 
-create sequence account_number_sequence;
+create sequence "account_number_sequence";
