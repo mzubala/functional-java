aaa5f5144004aa898bedaf4984c23e09348d4a42
Maciej Zubala <maciej.zubala@gmail.com>
2022-11-20 18:08:52 +0100

    lazy

---
+++ fun-basics/src/main/java/pl/com/bottega/functional/basics/lazy/Lazy.java
@@ -0,0 +1,29 @@
+package pl.com.bottega.functional.basics.lazy;
+
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class Lazy<T> {
+
+    public static <T> Lazy<T> of(Supplier<? extends T> supplier) {
+        return null;
+    }
+
+    public T get() {
+        return null;
+    }
+
+    public <S> Lazy<S> map(Function<T, S> mapper) {
+        return null;
+    }
+
+    public <S> Lazy<S> flatMap(Function<T, Lazy<S>> mapper) {
+        return null;
+    }
+
+    public Optional<T> filter(Predicate<T> tester) {
+        return Optional.empty();
+    }
+}
