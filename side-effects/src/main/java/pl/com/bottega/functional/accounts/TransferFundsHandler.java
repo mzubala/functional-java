c0c74455f4d59f9e2894ad101aa5941b297716f8
Maciej Zubala <maciej.zubala@gmail.com>
2023-11-26 23:13:06 +0100

    refactor

--- side-effects/src/main/java/pl/com/bottega/functional/accounts/TransferFundsHandler.java
+++ side-effects/src/main/java/pl/com/bottega/functional/accounts/TransferFundsHandler.java
@@ -5,6 +5,7 @@ import lombok.NonNull;
 import lombok.Value;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
 import static pl.com.bottega.functional.accounts.TransferFundsHandler.TransferCommand;
 
@@ -36,14 +37,11 @@ class DefaultTransferFundsHandler implements TransferFundsHandler {
         Mono.zip(
                 accountRepository.find(command.getSource()),
                 accountRepository.find(command.getDestination())
-            ).flatMapMany((accounts) ->
-                Flux.fromIterable(accounts.mapT1(
-                    sourceAccount -> sourceAccount.debit(command.getAmount()).get()
-                ).mapT2(
-                    targetAccount -> targetAccount.credit(command.getAmount()).get()
-                ))
-            ).cast(Account.class)
-            .flatMap(accountRepository::save)
-            .then().block();
+        ).flatMap((accounts) -> {
+            var saved = accounts
+                    .mapT1(source -> accountRepository.save(source.debit(command.getAmount()).get()))
+                    .mapT2(dest -> accountRepository.save(dest.credit(command.getAmount()).get()));
+            return saved.getT1().zipWith(saved.getT2());
+        }).block();
     }
 }
