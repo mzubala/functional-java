95e26fde6d561afd81ca3ef12368030da4b48fca
Maciej Zubala <maciej.zubala@gmail.com>
2023-11-26 15:10:54 +0100

    updated java version

--- side-effects/src/integration/java/pl/com/bottega/functional/accounts/IntegrationTest.java
+++ side-effects/src/integration/java/pl/com/bottega/functional/accounts/IntegrationTest.java
@@ -2,21 +2,17 @@ package pl.com.bottega.functional.accounts;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import pl.com.bottega.functional.App;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @ExtendWith(PostgresDbExtension.class)
-@SpringBootTest(
-        classes = {App.class, IntegrationTestConfig.class},
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
-@ContextConfiguration
+@SpringBootTest(webEnvironment = DEFINED_PORT)
 public @interface IntegrationTest {
 }
